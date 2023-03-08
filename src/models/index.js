4634a5f0633e4da29ccea37ae1700db73d158100
DanielSebasCM <danielsebascm@hotmail.com>
2023-03-08 10:40:20 -0600

    Connect sequalize to BD

--- src/models/index.js
+++ src/models/index.js
@@ -1,4 +1,10 @@
 const initModels = require("./init-models");
-const sequelize = require("sequelize");
+const Sequelize = require("sequelize");
+
+const sequelize = new Sequelize("hotware", "root", "hotware", {
+  host: "localhost",
+  dialect: "mariadb",
+  port: "3306",
+});
 
 module.exports = initModels(sequelize);
