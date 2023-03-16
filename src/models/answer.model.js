73042b9e06acd06374314cd80ea4e048892a96ac
leshcsr <a01708987@tec.mx>
2023-03-16 12:59:57 -0600

    changes answer.test

--- src/models/answer.model.js
+++ src/models/answer.model.js
@@ -28,23 +28,29 @@ class Answer {
     // Length of value is less than 400
     if (answer.value?.length > 400)
       throw new Error(
-        "Tu respuesta excede el número de caracteres permitidos."
+        "El tamaño de la respuesta debe ser menor a 400 caracteres"
       );
 
     // Value is not empty or null
     if (answer.value?.length == 0 || answer.value?.length == null) 
-      throw new Error("No puedes dejar estar pregunta sin contestar.");
+      throw new Error("Ingresa una respuesta");
+
+    //Uid is a number
+    if (isNaN(answer.uid))
+      throw new Error("Uid debe ser un número entero");
+    if (!answer.uid)
+      throw new Error("Uid no debe ser nulo");
 
     //Id_retrospective is a number
-    if (isNaN(suggested_todo.id_retrospective))
+    if (isNaN(answer.id_retrospective))
       throw new Error("id_retrospective debe ser un número entero");
-    if (!suggested_todo.id_retrospective)
+    if (!answer.id_retrospective)
       throw new Error("id_retrospective no debe ser nulo");
 
     //Id_question is a number
-    if (isNaN(suggested_todo.id_question))
+    if (isNaN(answer.id_question))
       throw new Error("id_question debe ser un número entero");
-    if (!suggested_todo.id_retrospective)
+    if (!answer.id_retrospective)
       throw new Error("id_question no debe ser nulo");
 
     return true;
--- src/testing/answer.test.js
+++ src/testing/answer.test.js
@@ -1,7 +1,7 @@
 test("test true", () => {
   expect(true).toBe(true);
 })
-/*const Answer = require("../models/answer.model");
+const Answer = require("../models/answer.model");
 
 // ------------------ VERIFIER ------------------
 test("Answer value is in range value.length < 400", () => {
@@ -21,31 +21,65 @@ test("Answer value is not empty", () => {
 });
 
 
+test("Answer has valid uid", () => {
+  expect(() => {
+    new Answer({
+      value: "a",
+      uid: "Esto no sirve",
+    });
+  }).toThrow("Uid debe ser un número entero");
+});
+
+test("Answer uid is not null", () => {
+  expect(() => {
+    new Answer({
+      value: "a",
+      uid: null,
+    });
+      throw new Error("Uid no debe ser nulo");
+    }).toThrow("");
+});
+
 test("Answer has valid id_question", () => {
   expect(() => {
     new Answer({
-      value: "a".repeat(401),
+      value: "a",
+      uid: 1,
       id_question: "Esto no sirve",
     });
-  }).toThrow("id_question inválido");
+  }).toThrow("id_question debe ser un número entero");
+});
+
+
+test("Answer has valid id_question", () => {
+  expect(() => {
+    new Answer({
+      value: "a",
+      uid: 1,
+      id_question: null,
+    });
+  }).toThrow("id_question no debe ser nulo");
 });
 
 test("Answer has valid id_retrospective", () => {
     expect(() => {
       new Answer({
-        value: "a".repeat(401),
+        value: "a",
+        uid: 1,
+        id_question: 1,
         id_retrospective: "Esto no sirve",
       });
-    }).toThrow("id_retrospective inválido");
+    }).toThrow("id_retrospective debe ser un número entero");
   });
 
-  test("Answer has valid uid", () => {
+  test("Answer has valid id_retrospective", () => {
     expect(() => {
       new Answer({
-        value: "a".repeat(401),
-        uid: "Esto no sirve",
+        value: "a",
+        uid: 1,
+        id_question: 1,
+        id_retrospective: null,
       });
-    }).toThrow("uid inválido");
+    }).toThrow("id_retrospective no debe ser nulo");
   });
-
-*/
+ 
