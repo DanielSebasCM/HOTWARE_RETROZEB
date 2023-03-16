fdc5d823554cf771a8d1609f0178268e3e50e2ca
danfupo03 <danfupo@outlook.com>
2023-03-16 13:13:31 -0600

    Test reviewed

--- src/testing/retrospective.test.js
+++ src/testing/retrospective.test.js
@@ -6,6 +6,8 @@ test("Retrospective name is in range name.length < 40", () => {
     new Retrospective({
       name: "a".repeat(41),
       start: "2021-01-01 00:00:00",
+      id_team: 1,
+      id_sprint: 1,
     });
   }).toThrow("El tamaño del nombre debe ser menor a 40 caracteres");
 });
@@ -15,6 +17,18 @@ test("Retrospective name is not empty", () => {
     new Retrospective({
       name: "",
       start: "2021-01-01 00:00:00",
+      id_team: 1,
+      id_sprint: 1,
+    });
+  }).toThrow("Ingresa un nombre");
+});
+
+test("Retrospective has a start date", () => {
+  expect(() => {
+    new Retrospective({
+      start: "2021-01-01 00:00:00",
+      id_team: 1,
+      id_sprint: 1,
     });
   }).toThrow("Ingresa un nombre");
 });
@@ -23,7 +37,8 @@ test("Retrospective has a start date", () => {
   expect(() => {
     new Retrospective({
       name: "a".repeat(40),
-      start: null,
+      id_team: 1,
+      id_sprint: 1,
     });
   }).toThrow("Ingresa una fecha de inicio");
 });
@@ -33,6 +48,8 @@ test("Retrospective has a valid start date", () => {
     new Retrospective({
       name: "a".repeat(40),
       start: "Esto no sirve",
+      id_team: 1,
+      id_sprint: 1,
     });
   }).toThrow("Formato de fecha inválido");
 });
@@ -43,16 +60,20 @@ test("Retrospective has a valid end date", () => {
       name: "a".repeat(40),
       start: "2021-01-01 00:00:00",
       end: "Esto no sirve",
+      id_team: 1,
+      id_sprint: 1,
     });
   }).toThrow("Formato de fecha inválido");
 });
 
-test("Retrospective has a valid dates", () => {
+test("Retrospective star_date is before end_date", () => {
   expect(() => {
     new Retrospective({
       name: "a".repeat(40),
       start: "2021-01-01 00:00:00",
-      end: "2020-01-01 00:00:00",
+      end: "2021-01-01 00:00:00",
+      id_team: 1,
+      id_sprint: 1,
     });
   }).toThrow("La fecha de inicio debe ser menor a la fecha de fin");
 });
@@ -64,23 +85,25 @@ test("Retrospective has a valid state", () => {
       start: "2021-01-01 00:00:00",
       end: "2021-01-01 00:00:00",
       state: "Esto no sirve",
+      id_team: 1,
+      id_sprint: 1,
     });
   }).toThrow("Estado inválido");
 });
 
-test("Retrospective has a valid id_team", () => {
+test("Retrospective has an id_team", () => {
   expect(() => {
     new Retrospective({
       name: "a".repeat(40),
       start: "2021-01-01 00:00:00",
       end: "2021-01-01 00:00:00",
       state: "active",
-      id_team: "Esto no sirve",
+      id_sprint: 1,
     });
   }).toThrow("Id de equipo inválido");
 });
 
-test("Retrospective has a valid id_sprint", () => {
+test("Retrospective has an id_sprint", () => {
   expect(() => {
     new Retrospective({
       name: "a".repeat(40),
@@ -88,7 +111,6 @@ test("Retrospective has a valid id_sprint", () => {
       end: "2021-01-01 00:00:00",
       state: "active",
       id_team: 1,
-      id_sprint: "Esto no sirve",
     });
   }).toThrow("Id de sprint inválido");
 });
