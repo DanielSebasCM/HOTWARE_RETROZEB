ed36cc31c6ff9ff1e0ad76a98380cb74f77385e9
DanielSebasCM <45676844+DanielSebasCM@users.noreply.github.com>
2023-03-17 14:54:09 -0600

    Update src/utils/db.js

--- src/utils/db.js
+++ src/utils/db.js
@@ -7,6 +7,7 @@ const pool = mysql.createPool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  port: process.env.PORT || 3306,
 });
 
 module.exports = pool.promise();
