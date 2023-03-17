206dfccb3e813b38508df1960e31155121ba981a
Ricardo Fernandez <ricardo.a.fernandez.a@gmail.com>
2023-03-17 14:43:36 -0600

    Added environment variables

--- src/utils/db.js
+++ src/utils/db.js
@@ -1,10 +1,12 @@
+require("dotenv").config();
 const mysql = require("mysql2");
 // create the connection
+
 const pool = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "hotware",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
 
 module.exports = pool.promise();
