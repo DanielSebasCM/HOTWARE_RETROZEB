df53413c28807541bc72574b0b8b3e436930b4d5
DanielSebasCM <danielsebascm@hotmail.com>
2023-03-24 10:52:16 -0600

    Moved parsing login to frontend script

--- public/assets/js/dashboard_metrics.js
+++ public/assets/js/dashboard_metrics.js
@@ -1,6 +1,45 @@
+let groupedByState = {};
+
+states.forEach((state) => {
+  let state_data = issues.filter((d) => d.state === state.label);
+  groupedByState[state.label] = state_data;
+});
+
+console.log(groupedByState);
+
+let data_general = {
+  label: "General",
+  data: [],
+};
+
+states.forEach((state) => {
+  const state_data = groupedByState[state.label]
+    .map((d) => d.story_points)
+    .reduce((a, b) => a + b, 0);
+
+  if (state_data) {
+    data_general.data.push(state_data);
+  } else {
+    data_general.data.push(0);
+  }
+});
 createFilteredChart("general-chart", "General", [data_general], states, true);
+
+const epics = [...new Set(issues.map((d) => d.epic_name))];
+const data_epics = parseGroupedIssues(groupedByState, "epic_name", epics);
 createFilteredChart("epics-chart", "Epics", data_epics, states, true);
+
+const types = [...new Set(issues.map((d) => d.type))];
+console.log(types);
+const data_types = parseGroupedIssues(groupedByState, "type", types);
 createFilteredChart("types-chart", "Types", data_types, states, true);
+
+const priorities = ["Lowest", "Low", "Medium", "High", "Highest"];
+const data_priorities = parseGroupedIssues(
+  groupedByState,
+  "priority",
+  priorities
+);
 createFilteredChart(
   "priorities-chart",
   "Priorities",
@@ -90,4 +129,30 @@ function createFilteredChart(
       },
     },
   });
+}
+
+function parseGroupedIssues(rawData, filter, filterValues, nullLabel) {
+  const data = [];
+  filterValues.forEach((filterValue) => {
+    let filterData = [];
+    states.forEach((state) => {
+      let stateData = rawData[state.label]
+        .filter((d) => d[filter] === filterValue)
+        .map((d) => d.story_points)
+        .reduce((a, b) => a + b, 0);
+      if (stateData) {
+        filterData.push(stateData);
+      } else {
+        filterData.push(0);
+      }
+    });
+    if (filterValue === null) {
+      filterValue = nullLabel || "N/A";
+    }
+    data.push({
+      label: filterValue,
+      data: filterData,
+    });
+  });
+  return data;
 }
--- src/controllers/dashboard.controller.js
+++ src/controllers/dashboard.controller.js
@@ -1,6 +1,6 @@
 const Retrospective = require("../models/retrospective.model");
 
-const states_colors = [
+const states = [
   { label: "To Do", color: "rgba(255, 99, 132, 0.6)" },
   { label: "En curso", color: "rgba(54, 162, 235, 0.6)" },
   { label: "Pull request", color: "rgba(255, 206, 86, 0.6)" },
@@ -23,83 +23,21 @@ const renderDashboardMetrics = async (req, res) => {
   let retrospective;
   try {
     retrospective = await Retrospective.getById(id_retrospective);
+    const issues = await retrospective.getIssues();
+
+    res.render("dashboard_metrics", {
+      title: "Dashboard Metricas",
+      user: "Hotware",
+      retrospective,
+      states,
+      issues,
+    });
   } catch (error) {
     console.log(error);
     return res.status(500).send("Id de retrospectiva no válido");
   }
-
-  // General metrics
-  const raw_data_general = await retrospective.getMetricsTotal();
-  let data_general = {
-    label: "Total",
-    data: [],
-  };
-  states_colors.forEach((state) => {
-    const state_data = raw_data_general.find((d) => d.state === state.label);
-    if (state_data) {
-      data_general.data.push(Number(state_data["Story Points"]));
-    } else {
-      data_general.data.push(0);
-    }
-  });
-
-  // Epics metrics
-  const raw_data_epics = await retrospective.getMetricsEpics();
-  const epics = new Set(raw_data_epics.map((d) => d.epic_name));
-  epics.add(null);
-  const data_epics = parseGroupedIssues(raw_data_epics, "epic_name", epics);
-  // replace null with "Sin epic"
-  data_epics.forEach((epic) => {
-    if (epic.label === null) {
-      epic.label = "Sin epic";
-    }
-  });
-
-  // Types metrics
-  const rawDataTypes = await retrospective.getMetricsTypes();
-  const types = new Set(rawDataTypes.map((d) => d.type));
-  const data_types = parseGroupedIssues(rawDataTypes, "type", types);
-
-  // Priorities metrics
-  const rawDataPriorities = await retrospective.getMetricsPriorities();
-  const priorities = ["Lowest", "Low", "Medium", "High", "Highest"];
-  const data_priorities = parseGroupedIssues(
-    rawDataPriorities,
-    "priority",
-    priorities
-  );
-
-  res.render("dashboard_metrics", {
-    title: "Dashboard Metricas",
-    user: "Hotware",
-    retrospective,
-    states: states_colors,
-    data_general,
-    data_epics,
-    data_types,
-    data_priorities,
-  });
 };
 
 module.exports = {
   renderDashboardMetrics: renderDashboardMetrics,
 };
-
-function parseGroupedIssues(rawData, filter, filterValues) {
-  const data = [];
-  filterValues.forEach((filterValue) => {
-    let filterData = [];
-    states_colors.forEach((state) => {
-      const state_data = rawData.find((d) => {
-        return d.state === state.label && d[filter] === filterValue;
-      });
-      if (state_data) {
-        filterData.push(Number(state_data["Story Points"]));
-      } else {
-        filterData.push(0);
-      }
-    });
-    data.push({ label: filterValue, data: filterData });
-  });
-  return data;
-}
