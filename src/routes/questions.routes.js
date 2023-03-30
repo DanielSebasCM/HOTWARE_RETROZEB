cb42d57cd3652223754f3ad88cd5d83ddf271204
danfupo03 <danfupo@outlook.com>
2023-03-30 17:25:33 -0600

    Post questions completed

--- src/routes/questions.routes.js
+++ src/routes/questions.routes.js
@@ -5,4 +5,8 @@ const controller = require("../controllers/question.controller");
 // RENDERING ROUTES
 router.get("/", controller.renderQuestions);
 
+// POSTING QUESTIONS
+router.get("/nueva", controller.renderNewQuestion);
+router.post("/nueva", controller.postQuestion);
+
 module.exports = router;
