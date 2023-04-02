7aaa443c77cc3a1b87757198b0fc9d450819e174
DanielSebasCM <danielsebascm@hotmail.com>
2023-04-02 15:51:15 -0600

    Privilege refactor

--- src/testing/privilege.test.js
+++ src/testing/privilege.test.js
@@ -1,9 +1,72 @@
 const Privilege = require("../models/privilege.model");
+const ValidationError = require("../errors/ValidationError");
+const validationMessages = require("../utils/messages").validation;
+const privilegeMaxLength =
+  require("../utils/constants").limits.privilegeMaxLength;
 
-/*test("privilege get all", async () => {
-  expect(await Privilege.getAll()).not.toThrow();
+test("Privilege id is an integer", () => {
+  let thrownError;
+  const expectedError = new ValidationError(
+    "id",
+    validationMessages.mustBeInteger
+  );
+  try {
+    new Privilege({
+      id: "1",
+      name: "test",
+    });
+  } catch (error) {
+    thrownError = error;
+  }
+  expect(thrownError).toEqual(expectedError);
 });
-*/
+
+test("Privilege has name", () => {
+  let thrownError;
+  const expectedError = new ValidationError(
+    "name",
+    validationMessages.isMandatory
+  );
+  try {
+    new Privilege({});
+  } catch (error) {
+    thrownError = error;
+  }
+  expect(thrownError).toEqual(expectedError);
+});
+
+test("Privilege name is not empty", () => {
+  let thrownError;
+  const expectedError = new ValidationError(
+    "name",
+    validationMessages.isMandatory
+  );
+  try {
+    new Privilege({
+      name: "",
+    });
+  } catch (error) {
+    thrownError = error;
+  }
+  expect(thrownError).toEqual(expectedError);
+});
+
+test(`Privilege name is not longer than ${privilegeMaxLength} characters`, () => {
+  let thrownError;
+  const expectedError = new ValidationError(
+    "name",
+    validationMessages.mustBeShorterThan(privilegeMaxLength)
+  );
+  try {
+    new Privilege({
+      name: "a".repeat(privilegeMaxLength + 1),
+    });
+  } catch (error) {
+    thrownError = error;
+  }
+  expect(thrownError).toEqual(expectedError);
+});
+
 test("privilege get by id", async () => {
   const privilege = await Privilege.getById(1);
   expect(privilege.id).toBe(1);
