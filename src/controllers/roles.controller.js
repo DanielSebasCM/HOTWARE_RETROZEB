2167e83d0897770a3814075d43d0cc8e1d1dac36
danfupo03 <danfupo@outlook.com>
2023-04-05 21:16:53 -0600

    Delete roles done, add roles more less...

--- src/controllers/roles.controller.js
+++ src/controllers/roles.controller.js
@@ -1,14 +1,52 @@
 const Role = require("../models/role.model");
+const Privilege = require("../models/privilege.model");
 
 const renderRoles = async (req, res, next) => {
   try {
     const roles = await Role.getAllActive();
-    res.render("roles", { roles, title: "Roles" });
+    const privileges = await Privilege.getAll();
+    res.render("roles", { roles, privileges, title: "Roles" });
+  } catch (err) {
+    next(err);
+  }
+};
+
+const deleteRole = async (req, res, next) => {
+  try {
+    const { id } = req.body;
+    const role = await Role.getById(id);
+    await role.delete();
+    req.session.successMessage = "Rol eliminado con éxito";
+    res.redirect("/roles");
+  } catch (err) {
+    next(err);
+  }
+};
+
+const renderNewRole = async (req, res, next) => {
+  try {
+    const privileges = await Privilege.getAll();
+    res.render("roles/new", { privileges, title: "Nuevo rol" });
+  } catch (err) {
+    next(err);
+  }
+};
+
+const postRole = async (req, res, next) => {
+  try {
+    const { name, privilege } = req.body;
+    const newRole = new Role({ name, privileges: privilege });
+    await newRole.post();
+    req.session.successMessage = "Rol creado con éxito";
+    res.redirect("/roles");
   } catch (err) {
     next(err);
   }
 };
 
 module.exports = {
-    renderRoles,
+  renderRoles,
+  deleteRole,
+  renderNewRole,
+  postRole,
 };
--- src/routes/roles.routes.js
+++ src/routes/roles.routes.js
@@ -5,4 +5,11 @@ const controller = require("../controllers/roles.controller");
 // RENDERING ROUTES
 router.get("/", controller.renderRoles);
 
+// POSTING ROLES
+router.get("/nuevo", controller.renderNewRole);
+router.post("/nuevo", controller.postRole);
+
+// DELETING ROLES
+router.delete("/", controller.deleteRole);
+
 module.exports = router;
