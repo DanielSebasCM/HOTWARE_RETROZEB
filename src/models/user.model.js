bac4932d7594ae4a8167749e3432e447dcd78f85
Damariz Licea <A01369045@itesm.mx>
2023-04-13 13:20:29 -0600

    Falta agregar botón de guardar cambios en modificar

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -3,6 +3,7 @@ const ValidationError = require("../errors/ValidationError");
 const validationMessages = require("../utils/messages").validation;
 const Team = require("./team.model");
 const Privilege = require("./privilege.model");
+const Role = require("./role.model");
 
 class User {
   constructor(user) {
@@ -71,6 +72,14 @@ class User {
     );
 
     return privileges.map((privilege) => new Privilege(privilege));
+  }
+
+  async getRoles() {
+    const [roles, _] = await db.execute(
+      "SELECT r.* FROM role r, users_roles ur WHERE ur.uid = ? AND ur.id_role = r.id",
+      [this.uid]
+    );
+    return roles.map((role) => new Role(role));
   }
 
   static async getByGoogleId(id_google_auth) {
---
+++ src/routes/user.routes.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const router = express.Router();
+const controller = require("../controllers/user.controller");
+const authorize = require("../middlewares/privilege");
+const privileges = require("../utils/constants").privileges.users;
+
+// RENDERING ROUTES
+router.get("/", authorize([privileges.getUsers]), controller.renderUsers);
+
+router.delete(
+  "/:uid/borrar",
+  authorize([privileges.deleteUsers]),
+  controller.deleteUser
+);
+
+router.get(
+  "/:uid/modificar",
+  authorize([privileges.canModifyUsers]),
+  controller.modifyUser
+);
+
+module.exports = router;
