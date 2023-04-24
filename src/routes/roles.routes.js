5fdac137caabfefdbad38d91aa6654a4fd4d997f
danfupo03 <danfupo@outlook.com>
2023-04-24 16:51:44 -0600

    Finished RBAC control

--- src/routes/roles.routes.js
+++ src/routes/roles.routes.js
@@ -5,17 +5,37 @@ const authorize = require("../middlewares/privilege");
 const privileges = require("../utils/constants").privileges.roles;
 
 // RENDERING ROUTES
-router.get("/", authorize([privileges.getRoles]) ,controller.renderRoles);
+router.get("/", authorize([privileges.getRoles]), controller.renderRoles);
 
 // POSTING ROLES
-router.get("/nuevo", authorize([privileges.canCreateRoles]) ,controller.renderNewRole);
-router.post("/nuevo", authorize([privileges.canCreateRoles]) ,controller.postRole);
+router.get(
+  "/nuevo",
+  authorize([privileges.canCreateRoles]),
+  controller.renderNewRole
+);
+router.post(
+  "/nuevo",
+  authorize([privileges.canCreateRoles]),
+  controller.postRole
+);
 
 // MODIFYING ROLES
-router.get("/:id/modificar", authorize([privileges.canModifyRoles]) ,controller.renderModifyRole);
-router.patch("/:id/modificar", authorize([privileges.canModifyRoles]), controller.modifyRole);
+router.get(
+  "/:id/modificar",
+  authorize([privileges.canModifyRoles]),
+  controller.renderModifyRole
+);
+router.patch(
+  "/:id/modificar",
+  authorize([privileges.canModifyRoles]),
+  controller.modifyRole
+);
 
 // DELETING ROLES
-router.delete("/:id/eliminar", authorize([privileges.canDeleteRoles]) ,controller.deleteRole);
+router.delete(
+  "/:id/eliminar",
+  authorize([privileges.canDeleteRoles]),
+  controller.deleteRole
+);
 
 module.exports = router;
