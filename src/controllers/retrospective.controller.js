d869ae3089b1c2b9ee786c09abc77e2aac37a936
Ricardo Fernandez <ricardo.a.fernandez.a@gmail.com>
2023-04-25 22:54:15 -0600

    Fixed Allowing to answer Retro twice

--- src/controllers/retrospective.controller.js
+++ src/controllers/retrospective.controller.js
@@ -38,28 +38,30 @@ const renderInitRetrospective = async (req, res, next) => {
       return res.redirect(".");
     }
 
-    await Sprint.syncJira()
+    await Sprint.syncJira();
 
     const team = await Team.getById(req.session.selectedTeam.id);
     let questions = [];
     const retrospective = await team.getLastRetrospective();
-      
+
     if (retrospective && retrospective.state == "IN_PROGRESS") {
       req.session.errorMessage =
         "Ya hay una retrospectiva activa para el equipo " +
-        team.name + ". Ciérrala para empezar una nueva."
+        team.name +
+        ". Ciérrala para empezar una nueva.";
       return res.redirect(".");
     }
-    
+
     let sprint = await Sprint.getLast();
 
-    if(retrospective && retrospective.id_sprint == sprint.id) {
+    if (retrospective && retrospective.id_sprint == sprint.id) {
       req.session.errorMessage =
-        "El equipo " + team.name + " ya tiene una retrospectiva creada para el último sprint."
+        "El equipo " +
+        team.name +
+        " ya tiene una retrospectiva creada para el último sprint.";
       return res.redirect(".");
-
     }
-    
+
     questions = await Question.getAll();
 
     const activeSprint = await Sprint.getLastWithRetroByTeamId(
@@ -205,10 +207,15 @@ const renderRetrospectiveMetrics = async (req, res, next) => {
 
     const team = await Team.getById(retrospective.id_team);
     retrospective.team_name = team.name;
+
     const questions = await retrospective.getQuestions();
-    let answer = await retrospective.getAnswers(questions[0]);
-    answer = answer.filter((a) => a.uid === req.session.currentUser.uid);
-    let answered = answer.length ? true : false;
+    for (let question of questions) {
+      question.answers = await retrospective.getAnswers(question);
+      question.answers = question.answers.filter(
+        (answer) => answer.uid === req.session.currentUser.uid
+      );
+    }
+    const answered = questions.some((question) => question.answers.length > 0);
     const teamUsers = await retrospective.getUsers();
     let isMember = teamUsers.some(
       (user) => user.uid === req.session.currentUser.uid
@@ -244,9 +251,14 @@ const renderRetrospectiveAnswer = async (req, res, next) => {
       return res.redirect(`/retrospectivas/${retrospectiveId}/preguntas`);
     }
     const questions = await retrospective.getQuestions();
-    let answer = await retrospective.getAnswers(questions[0]);
-    answer = answer.filter((a) => a.uid === req.session.currentUser.uid);
-    if (answer.length > 0) {
+    for (let question of questions) {
+      question.answers = await retrospective.getAnswers(question);
+      question.answers = question.answers.filter(
+        (answer) => answer.uid === req.session.currentUser.uid
+      );
+    }
+    const answered = questions.some((question) => question.answers.length > 0);
+    if (answered) {
       req.session.errorMessage = "Ya respondiste esta retrospectiva";
       res.redirect(`/retrospectivas/${retrospectiveId}/preguntas`);
     } else {
